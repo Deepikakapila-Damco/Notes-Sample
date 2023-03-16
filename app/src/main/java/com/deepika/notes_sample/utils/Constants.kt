56064cb34dc2d4aea99e9e7d955a451f7a0ee664
swarandamco <swarans@damcogroup.com>
2023-03-16 14:18:09 +0530

    Updated readme

--- app/src/main/java/com/deepika/notes_sample/utils/Constants.kt
+++ app/src/main/java/com/deepika/notes_sample/utils/Constants.kt
@@ -5,7 +5,6 @@ object Constants {
     const val BASE_URL = "https://notes-api-sample.herokuapp.com/"
     const val USER_TOKEN = "user_token"
     const val PREFS_TOKEN_FILE = "prefs_token_file"
-    val BG_COLORS = arrayOf("#fef9ed", "#f2f8ff", "#edffee", "#fce9ff")
 
     const val NOTE_TABLE= "note_table"
     const val NOTE_DATABASE= "note_database"
